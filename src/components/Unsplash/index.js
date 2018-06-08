54f75e0e1cf9d5b6d2d6e6fc766551a34e556ce3
Alberto Menendez Romero <albertomr86@gmail.com>
2018-06-07 22:42:17 -0300

    Plugin interactions

---
+++ src/components/Unsplash/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Unsplash extends React.PureComponent {
+  render () {
+    return (
+      <div/>
+    );
+  }
+}
+
+export default Unsplash;
--- src/createUnsplashPlugin.js
+++ src/createUnsplashPlugin.js
@@ -1,9 +1,19 @@
 import decorateComponentWithProps from 'decorate-component-with-props';
-import { UnsplashButton } from './components';
+import { UnsplashButton, UnsplashExplorer, Unsplash } from './components';
+import { addBlock, addAtomicBlock, removeBlock } from './modifiers';
+import { buildSearchPhotosUrl } from './utils';
 import defaultTheme from './plugin.css';
 
 const ATOMIC = 'atomic';
-const defaultOptions = {};
+const defaultOptions = {
+  accessKey: null,
+  placeholder: 'Type to search Unsplash, and press Enter',
+  
+  perPage: 9,
+  onRequest: async (url) => {
+    return (await fetch(url)).json();
+  }
+};
 
 export default ({
   theme = {},
@@ -15,20 +25,56 @@ export default ({
 } = {}) => {
 
   // Modifiers.
-  const addUnsplashExplorer = (editorState, data = {}) => {};
-  const addUnsplash = (editorState, data = {}) => {};
+  const addUnsplashExplorer = (editorState, data = {}) => addBlock(editorState, explorerType, data);
+  const addUnsplash = (editorState, data = {}) => addAtomicBlock(editorState, unsplashType, data);
 
   // Plugin.
   const pluginOptions = Object.assign({}, defaultOptions, options);
   const pluginTheme = Object.assign({}, defaultTheme, theme);
 
+  const ThemedExplorer = decorateComponentWithProps(UnsplashExplorer, { theme: pluginTheme });
+  const DecoratedUnsplash = decorator(unsplashComponent);
+  const ThemedUnsplash = decorateComponentWithProps(DecoratedUnsplash, { theme: pluginTheme });
+
   return {
     blockRendererFn: (block, { getEditorState, setEditorState, setReadOnly }) => {
+      if (block.getType () === ATOMIC) {
+      }
+      else if (block.getType() === explorerType) {
+        return {
+          component: ThemedExplorer,
+          editable: false,
+          props: {
+            placeholder: pluginOptions.placeholder,
+            setReadOnly,
+
+            // When cancel the action.
+            onCancel: (block) => {
+              setEditorState(removeBlock(getEditorState(), block.key));
+            },
+
+            // When explore the gallery.
+            // Search and navigation.
+            onSearch: async (block, text, page) => {
+              const url = buildSearchPhotosUrl(pluginOptions.accessKey, text, page, pluginOptions.perPage);
+
+              return pluginOptions.onRequest(url);
+            },
+
+            // When select the picture.
+            onSelect: (block, data) => {
+              let editorState = removeBlock(getEditorState(), block.key);
+              editorState = addUnsplash(editorState, data);
+              setEditorState(editorState);
+            }
+          }
+        };
+      }
     },
 
     UnsplashButton: decorateComponentWithProps(UnsplashButton, {
       entityType: explorerType,
-      addUnsplash
+      addUnsplash: addUnsplashExplorer
     }),
 
     addUnsplashExplorer,
